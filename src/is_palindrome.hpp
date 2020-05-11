c35c21c07ca742a32a01195e4e02ead1950c1320
kuchichan <pawel.kucharski@hotmail.com>
2020-05-11 17:38:04 +0200

    signature fixed

--- src/is_palindrome.hpp
+++ src/is_palindrome.hpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 
 inline
-double isPalindrome(const std::string & text) {
+bool isPalindrome(const std::string & text) {
   std::string temp_1 = text;
   std::string temp_2 = text;
   std::reverse(temp_1.begin(), temp_1.end());
